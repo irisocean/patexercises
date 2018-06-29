c34b153f77d3d0450ad0e514cb18eec6ac19a08f
xushan <xushan123>
2018-06-29 11:02:25 +0800

    划拳

---
+++ B1026.cpp
@@ -0,0 +1,18 @@
+// B1026.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<stdio.h>
+int main(){
+	int c1,c2;
+	scanf("%d%d",&c1,&c2);
+	int ans = c2-c1;
+	if(ans%100>=50){
+		ans = ans/100+1;
+	}
+	else{
+		ans = ans/100;
+	}
+	printf("%.2d:%.2d:%.2d\n",ans/3600,ans%3600/60,ans%60);
+	return 0;
+}
