e4c4567089add345ffd4ac13c0854668c7a50bc8
xushan <xushan123>
2018-07-11 16:14:31 +0800

    输出U型图案

---
+++ A1031.cpp
@@ -0,0 +1,36 @@
+// A1031.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<cstdio>
+#include<cstring>
+int main(){
+	char str[81],ans[31][31];
+	gets(str);
+	int N=strlen(str);
+	int n1=(N+2)/3,n3=n1,n2=N+2-n1-n3;
+	for(int i=1;i<=n1;i++){
+		for(int j=1;j<=n2;j++){
+			ans[i][j]=' ';
+		}
+	}
+	int pos=0;
+	for(int i=1;i<=n1;i++){
+		ans[i][1]=str[pos++];
+	}
+	for(int i=2;i<=n2;i++){
+		ans[n1][i]=str[pos++];
+	}
+	for(int i=n1-1;i>=1;i--){
+		ans[i][n2]=str[pos++];
+	}
+	for(int i=1;i<=n1;i++){
+		for(int j=1;j<=n2;j++){
+			printf("%c",ans[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+
