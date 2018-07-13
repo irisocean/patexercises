c85666917bc67d655b48b8899980c95d664cdffe
xushan <xushan123>
2018-07-13 09:42:58 +0800

    两种说反话的写法

--- B1009.cpp
+++ B1009.cpp
@@ -16,4 +16,31 @@ int main(){
 	printf("\n");
 	return 0;
 }
-
+/*#include<stdio.h>
+#include<string.h>
+int main(){
+	char str[90];
+	gets(str);
+	int len =strlen(str),r=0,h=0;
+	char ans[90][90];
+	for(int i=0;i<len;i++){
+		if(str[i]!=' '){
+			ans[r][h++]=str[i];
+		}else{
+			ans[r][h] = '\0';
+			r++;
+			h=0;
+		}
+	}
+	ans[r][h] = '\0';
+	for(int i=r;i>=0;i--){
+		printf("%s",ans[i]);
+		if(i>0){
+			printf(" ");
+		}
+		else{
+			printf("\n");
+		}
+	}
+	return 0;
+}*/
