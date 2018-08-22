688dd144463ff3ee58c29991dae85047d4b2d945
xushan <xushan123>
2018-08-22 16:35:27 +0800

    每个学生都选了什么课

---
+++ A1039.cpp
@@ -0,0 +1,45 @@
+// A1039.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<cstdio>
+#include<cstring>
+#include<vector>
+#include<algorithm>
+using namespace std;
+const int N=40010;
+const int M=26*26*26*10+1;
+vector<int>selectCourse[M];
+int getId(char name[]){
+	int id=0;
+	for(int i=0;i<3;i++){
+		id=id*26+(name[i]-'A');
+	}
+	id=id*10+(name[3]-'0');
+	return id;
+}
+int main(){
+	char name[5];
+	int n,k;
+	scanf("%d%d",&n,&k);
+	for(int i=0;i<k;i++){
+		int course,x;
+		scanf("%d%d",&course,&x);
+		for(int j=0;j<x;j++){
+			scanf("%s",name);
+			int id=getId(name);
+			selectCourse[id].push_back(course);
+		}
+	}
+	for(int i=0;i<n;i++){
+		scanf("%s",name);
+		int id=getId(name);
+		sort(selectCourse[id].begin(),selectCourse[id].end());
+		printf("%s %d",name,selectCourse[id].size());
+		for(int j=0;j<selectCourse[id].size();j++){
+			printf(" %d",selectCourse[id][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
