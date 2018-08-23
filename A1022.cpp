68d9ea24a88450feb098d9a94995cdefbde47639
xushan <xushan123>
2018-08-23 16:34:01 +0800

    数字图书馆

---
+++ A1022.cpp
@@ -0,0 +1,56 @@
+// A1022.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<cstdio>
+#include<iostream>
+#include<map>
+#include<string>
+#include<set>
+using namespace std;
+map<string,set<int>>mpTitle,mpAuthor,mpKey,mpPub,mpYear;
+void query(map<string,set<int>>& mp,string& str){
+	if(mp.find(str)!=mp.end()){
+		for(set<int>::iterator it=mp[str].begin();it!=mp[str].end();it++){
+			printf("%07d\n",*it);
+		}
+	}
+	else{
+		printf("Not Found\n");
+	}
+}
+int main(){
+	int n,m,id,type;
+	string title,author,key,pub,year;
+	scanf("%d",&n);
+	for(int i=0;i<n;i++){
+		scanf("%d",&id);
+		char c=getchar();
+		getline(cin,title);
+		mpTitle[title].insert(id);
+		getline(cin,author);
+		mpAuthor[author].insert(id);
+		while(cin>>key){
+			mpKey[key].insert(id);
+			c=getchar();
+			if(c=='\n')break;
+		}
+		getline(cin,pub);
+		mpPub[pub].insert(id);
+		getline(cin,year);
+		mpYear[year].insert(id);
+	}
+	scanf("%d",&m);
+	string temp;
+	for(int i=0;i<m;i++){
+		scanf("%d: ",&type);
+		getline(cin,temp);
+		cout<<type<<": "<<temp<<endl;
+		if(type==1)query(mpTitle,temp);
+		else if(type==2)query(mpAuthor,temp);
+		else if(type==3)query(mpKey,temp);
+		else if(type==4)query(mpPub,temp);
+		else query(mpYear,temp);
+	}
+	return 0;
+}
