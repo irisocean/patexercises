0e02c78bcf551e830b2f6d7cbaeff82f9b53afc2
xushan <xushan123>
2018-08-24 15:30:21 +0800

    按数据从小到大调整链表顺序

---
+++ A1052.cpp
@@ -0,0 +1,54 @@
+// A1052.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<cstdio>
+#include<algorithm>
+using namespace std;
+const int maxn=100010;
+struct Node{
+	int address,data,next;
+	bool flag;
+}node[maxn];
+bool cmp(Node a,Node b){
+	if(a.flag==false||b.flag==false)return a.flag>b.flag;
+	else return a.data<b.data;
+}
+int main(){
+	for(int i=0;i<maxn;i++){
+		node[i].flag=false;
+	}
+	int n,address,begin;
+	scanf("%d%d",&n,&begin);
+	for(int i=0;i<n;i++){
+		scanf("%d",&address);
+		scanf("%d%d",&node[address].data,&node[address].next);
+		node[address].address=address;
+	}
+	int count=0,p=begin;
+	while(p!=-1){
+		node[p].flag=true;
+		count++;
+		p=node[p].next;
+	}
+	if(count==0)printf("0 -1\n");
+	else{
+		sort(node,node+maxn,cmp);
+		for(int i=0;i<count;i++){
+			if(i!=count-1){
+				node[i].next=node[i+1].address;
+			}else{
+				node[i].next=-1;
+			}
+		}
+		printf("%d %05d\n",count,node[0].address);
+		for(int i=0;i<count;i++){
+			if(i!=count-1){
+				printf("%05d %d %05d\n",node[i].address,node[i].data,node[i].next);
+			}else{
+				printf("%05d %d -1\n",node[i].address,node[i].data);
+			}
+		}
+	}
+	return 0;
+}
